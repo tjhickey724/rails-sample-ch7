340535078db2a899bc1d8b4c27d2dcafecc2dd36
tim <tjhickey@brandeis.edu>
2012-06-14 19:53:22 -0400

    first init

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User
+  attr_accessor :name, :email
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsCh7::Application.config.secret_token = 'a3c11112243e8e15eba1d5b51c305f53f8fa924df2a43256388323f5e8a81d0b2e4993e0eb1941890ab8ad3e797ac13a1646eee80eb876fc65dc73ff36c7c105'
